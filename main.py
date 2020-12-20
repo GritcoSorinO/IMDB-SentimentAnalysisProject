8e78c3145e2b8c21cb38b7ac3d14b20c59a4496e
GritcoSorinO <gritco.sorin@mail.ru>
2020-12-20 07:29:47 +0200

    Preprocess the review text and first and apply Random Forest classifier.

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
-
+import csv
+import pandas as pd
+from preprocessing import preprocess_the_text
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 if __name__ == '__main__':
-   x = 2
+   filepath = 'data/labeledTrainData.tsv'
+   tsv_file = open(filepath, encoding='utf-8')
+   read_tsv = csv.reader(tsv_file, delimiter='\t')
+   df_list = list()
 
-   x = x + 2
-   print(x)
+   for row in read_tsv:
+      df_list.append(row)
+   df = pd.DataFrame(df_list[1:], columns = df_list[0])
+
+   df['review'] = df['review'].apply(preprocess_the_text)
+
+   vectorizer = CountVectorizer(min_df=0, lowercase=False)
+   vectorizer.fit(df['review'])
+
+   reviews = df['review'].values
+   sentiments = df['sentiment'].values
+
+   test_size_ = 0.2
+   random_seed = 42
+
+   reviews_train, review_test, y_train, y_test = train_test_split(reviews,
+                                                                  sentiments,
+                                                                  test_size=test_size_,
+                                                                  random_state = random_seed)
+   X_train = vectorizer.transform(reviews_train)
+   X_test = vectorizer.transform(review_test)
+
+   rf_classifier = RandomForestClassifier(n_estimators=200)
+   print("Machine is Learning\n");
+   rf_classifier.fit(X_train, y_train)
+   score = rf_classifier.score(X_test, y_test)
+   print("Accuracy:", score)
+
+
---
+++ preprocessing.py
@@ -0,0 +1,20 @@
+import re
+from bs4 import BeautifulSoup
+from gensim.parsing.preprocessing import remove_stopwords
+
+
+def eliminate_html_tags(review_text):
+    soup = BeautifulSoup(review_text, 'html.parser')
+    return soup.get_text(separator=' ')
+
+def eliminate_special_characters(review_text):
+    pattern = r'[^a-zA-Z\s]'
+    return re.sub(pattern, ' ', review_text)
+
+
+def preprocess_the_text(review_text):
+    review_text = eliminate_html_tags(review_text)
+    review_text = eliminate_special_characters(review_text)
+    review_text = review_text.lower()
+    review_text = remove_stopwords(review_text)
+    return review_text
